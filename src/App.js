fb4cbbfef4306ed2c39b0652989eb139ceec4587
Russell Osborne <russell@burningpony.com>
2017-05-06 22:37:58 -0400

    Added support for OZ and negative numbers

--- src/App.js
+++ src/App.js
@@ -1,48 +1,52 @@
-import React, { Component } from 'react';
-import './App.css'
+import React, { Component } from "react";
+import "./App.css";
 class App extends Component {
-
   constructor(props) {
     super(props);
 
     this.USB_FILTERS = [
       { vendorId: 0x0922, productId: 0x8003 },
       { vendorId: 0x0922, productId: 0x8004 }
-    ]
+    ];
+
+    this.UNIT_MODES = { 2: "g", 11: "oz" };
+    this.SCALE_STATES = { 2: "±", 4: "+", 5: "-" };
 
     this.state = {
       connected: false,
       device: null,
       shouldRead: null,
-      grams: 0
-    }
+      weight: 0,
+      unit: "g",
+      scaleState: ''
+    };
 
     if (navigator.usb) {
-      navigator.usb.getDevices({ filters: this.USB_FILTERS })
-        .then((devices) => {
-          devices.forEach(device => {
-            this.bindDevice(device)
-          });
+      navigator.usb.getDevices({ filters: this.USB_FILTERS }).then(devices => {
+        devices.forEach(device => {
+          this.bindDevice(device);
         });
-
-      navigator.usb.addEventListener('connect', e => {
-        console.log('device connected', e);
-        this.bindDevice(e.device)
       });
 
-      navigator.usb.addEventListener('disconnect', e => {
-        console.log('device lost', e);
-        this.setState({ connected: false, device: null })
+      navigator.usb.addEventListener("connect", e => {
+        console.log("device connected", e);
+        this.bindDevice(e.device);
+      });
+
+      navigator.usb.addEventListener("disconnect", e => {
+        console.log("device lost", e);
+        this.setState({ connected: false, device: null });
       });
 
       this.connect = () => {
-        navigator.usb.requestDevice({ filters: this.USB_FILTERS })
+        navigator.usb
+          .requestDevice({ filters: this.USB_FILTERS })
           .then(device => this.bindDevice(device))
           .catch(error => {
-            console.error(error)
-            this.setState({ connected: false, device: null })
+            console.error(error);
+            this.setState({ connected: false, device: null });
           });
-      }
+      };
     }
 
     this.getWeight = this.getWeight.bind(this);
@@ -51,25 +55,35 @@ class App extends Component {
   }
 
   getWeight() {
-    this.setState({ shouldRead: true })
+    this.setState({ shouldRead: true });
     const { device } = this.state;
-    const { endpointNumber, packetSize } = device.configuration.interfaces[0].alternate.endpoints[0]
+    const { endpointNumber, packetSize } = device.configuration.interfaces[
+      0
+    ].alternate.endpoints[0];
     let readLoop = () => {
-
-      device.transferIn(endpointNumber, packetSize)
+      device
+        .transferIn(endpointNumber, packetSize)
         .then(result => {
-          let data = new Uint8Array(result.data.buffer)
-          let grams = data[4] + (256 * data[5])
-          this.setState({ grams: grams })
+          let data = new Uint8Array(result.data.buffer);
+          let weight = (data[4] + 256 * data[5]);
+          const unitMode = data[2];
+          const scaleState = data[1];
+          console.log("data", data);
+
+          this.setState({
+            weight: weight,
+            unit: this.UNIT_MODES[unitMode],
+            scaleState: this.SCALE_STATES[scaleState]
+          });
 
           if (this.state.shouldRead) {
             readLoop();
           }
         })
-        .catch((err) => {
-          console.error('USB Read Error', err)
-        })
-    }
+        .catch(err => {
+          console.error("USB Read Error", err);
+        });
+    };
     readLoop();
   }
 
@@ -78,23 +92,27 @@ class App extends Component {
   }
 
   bindDevice(device) {
-    device.open()
+    device
+      .open()
       .then(() => {
-        console.log(`Connected ${device.productName} ${device.manufacturerName}`, device);
-        this.setState({ connected: true, device: device })
+        console.log(
+          `Connected ${device.productName} ${device.manufacturerName}`,
+          device
+        );
+        this.setState({ connected: true, device: device });
 
         if (device.configuration === null) {
           return device.selectConfiguration(1);
         }
       })
       .then(() => device.claimInterface(0))
-      .catch((err) => {
-        console.error('USB Error', err)
-      })
+      .catch(err => {
+        console.error("USB Error", err);
+      });
   }
 
   render() {
-    const { connected, shouldRead, grams } = this.state
+    const { connected, shouldRead, weight, unit, scaleState } = this.state;
     return (
       <main>
         <h1>
@@ -102,20 +120,19 @@ class App extends Component {
         </h1>
 
         {!navigator.usb &&
-          <p>Please enable chrome://flags/#enable-experimental-web-platform-features</p>
-        }
+          <p>
+            Please enable chrome://flags/#enable-experimental-web-platform-features
+          </p>}
 
-        {connected && !shouldRead &&
-          <button onClick={this.getWeight}>Get Scale Weight</button>
-        }
+        {connected &&
+          !shouldRead &&
+          <button onClick={this.getWeight}>Get Scale Weight</button>}
 
-        {shouldRead &&
-          <button onClick={this.stopWeight}>Hold</button>
-        }
+        {shouldRead && <button onClick={this.stopWeight}>Hold</button>}
 
         <button onClick={this.connect}>Register Device</button>
 
-        <h2>{grams}g</h2>
+        <h2>{scaleState}{weight}{unit}</h2>
       </main>
     );
   }
